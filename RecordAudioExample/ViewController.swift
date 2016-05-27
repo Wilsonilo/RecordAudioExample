bfcdbdc3fd76c5954fcbb8de968324b947ff629c
Wilson Muñoz <contacto@maquilaweb.net>
2016-05-27 10:26:33 -0500

    Initial Commit

--- RecordAudioExample/ViewController.swift
+++ RecordAudioExample/ViewController.swift
@@ -1,25 +1,139 @@
 //
-//  ViewController.swift
+//
 //  RecordAudioExample
 //
-//  Created by Wilson Muñoz on 5/27/16.
+//  Created by Wilson Muñoz on 5/26/16.
 //  Copyright © 2016 Wilson Muñoz. All rights reserved.
 //
 
 import UIKit
+import AVFoundation
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, AVAudioPlayerDelegate, AVAudioRecorderDelegate {
+    
+    @IBOutlet weak var ButttonRecord: UIButton!
+    @IBOutlet weak var ButtonPlay: UIButton!
+    
+    
+    var soundRecorder : AVAudioRecorder!
+    var SoundPlayer : AVAudioPlayer!
+    
+    var AudioFileName = "sound.m4a"
+    
+    
+    let recordSettings = [AVSampleRateKey : NSNumber(float: Float(44100.0)),
+                          AVFormatIDKey : NSNumber(int: Int32(kAudioFormatMPEG4AAC)),
+                          AVNumberOfChannelsKey : NSNumber(int: 1),
+                          AVEncoderAudioQualityKey : NSNumber(int: Int32(AVAudioQuality.Medium.rawValue))]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        setupRecorder()
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
+    
+    @IBAction func ActionRecord(sender: AnyObject) {
+        if sender.titleLabel?!.text == "Record"{
+            
+            soundRecorder.record()
+            sender.setTitle("Stop", forState: .Normal)
+            ButtonPlay.enabled = false
+            
+        }
+        else{
+            
+            soundRecorder.stop()
+            sender.setTitle("Record", forState: .Normal)
+            ButtonPlay.enabled = true
+        }
+        
+    }
+    @IBAction func ActionPlay(sender: AnyObject) {
+        
+        if sender.titleLabel?!.text == "Play" {
+            
+            ButttonRecord.enabled = false
+            sender.setTitle("Stop", forState: .Normal)
+            
+            preparePlayer()
+            SoundPlayer.play()
+            
+        }
+        else{
+            
+            SoundPlayer.stop()
+            sender.setTitle("Play", forState: .Normal)
+            
+        }
+        
+    }
+    
+    //HELPERS
+    
+    func preparePlayer(){
+        
+        do {
+            try SoundPlayer = AVAudioPlayer(contentsOfURL: directoryURL()!)
+            SoundPlayer.delegate = self
+            SoundPlayer.prepareToPlay()
+            SoundPlayer.volume = 1.0
+        } catch {
+            print("Error playing")
+        }
+        
+    }
+    
+    func setupRecorder(){
+        
+        
+        
+        let audioSession:AVAudioSession = AVAudioSession.sharedInstance()
+        
+        //ask for permission
+        if (audioSession.respondsToSelector(#selector(AVAudioSession.requestRecordPermission(_:)))) {
+            AVAudioSession.sharedInstance().requestRecordPermission({(granted: Bool)-> Void in
+                if granted {
+                    print("granted")
+                    
+                    //set category and activate recorder session
+                    do {
+                        
+                        try audioSession.setCategory(AVAudioSessionCategoryPlayAndRecord)
+                        try self.soundRecorder = AVAudioRecorder(URL: self.directoryURL()!, settings: self.recordSettings)
+                        self.soundRecorder.prepareToRecord()
+                        
+                    } catch {
+                        
+                        print("Error Recording");
+                        
+                    }
+                    
+                }
+            })
+        }
+        
+    }
+    
+    func directoryURL() -> NSURL? {
+        let fileManager = NSFileManager.defaultManager()
+        let urls = fileManager.URLsForDirectory(.DocumentDirectory, inDomains: .UserDomainMask)
+        let documentDirectory = urls[0] as NSURL
+        let soundURL = documentDirectory.URLByAppendingPathComponent("sound.m4a")
+        return soundURL
+    }
+    
+    func audioRecorderDidFinishRecording(recorder: AVAudioRecorder, successfully flag: Bool) {
+        ButtonPlay.enabled = true
+    }
+    
+    func audioPlayerDidFinishPlaying(player: AVAudioPlayer, successfully flag: Bool) {
+        ButttonRecord.enabled = true
+        ButtonPlay.setTitle("Play", forState: .Normal)
+    }
+    
 }
-
